abdb86d01315e6f84c3c8b2be1dc868cbf12a15b
Jose Egberto <egbertojap@gmail.com>
2025-02-03 21:39:56 -0300

    Atualizando a questão três

--- bloco_um/3_exercicio.py
+++ bloco_um/3_exercicio.py
@@ -7,7 +7,7 @@ materia_tres = float(input('Ética e Legislação em Enfermagem : '))
 
 media = (materia_um + materia_dois + materia_tres) / 3
 situacao = ''
-if media >= 7:
+if materia_um >= 7 and materia_dois >= 7 and materia_tres >= 7:
     situacao = 'APROVADO'
 else:
     situacao = 'REPROVADO'
@@ -16,5 +16,4 @@ print('\n'+ ('*' * 22) + 'BOLETIM' + ('*' * 22) + '\n\n'
       f'Anatomia e Fisiologia {"-" * 21} {materia_um:.2f}\n'
       f'Procedimentos de Enfermagem {"-" * 15} {materia_dois:.2f}\n'
       f'Ética e Legislação em Enfermagem {"-" * 10} {materia_tres:.2f}\n\n'
-      f'Média {"-" * 37} {media:.2f}\n'
       f'Situação: {situacao}')
