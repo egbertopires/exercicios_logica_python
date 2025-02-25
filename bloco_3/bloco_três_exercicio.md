d494319b5eb54cd5f78239365d8aad4afdc0ec78
Egberto Pires <egbertojao@gmail.com>
2025-02-24 22:03:58 -0300

    Subindo os exercícios 1 e 2 do bloco três

---
+++ bloco_3/bloco_três_exercicio.md
@@ -0,0 +1,12 @@
+# 1_exercício:
+"Crie uma função em Python que receba um número inteiro e retorne um intervalo contendo os dois números antecessores
+e os dois sucessores desse número.
+
+# 2_exercicio:
+Fatorial é um número inteiro positivo, representado por n!
+Calculamos a fatorial pela multiplicação desse número n por todos os seus antecessores até chegar em 1. 
+Ainda, fatorial de 0! Sempre será 1.
+
+Considerando a breve explicação sobre fatorial, escreva uma função que calcule o fatorial de um número recebido 
+como parâmetro e retorne o seu resultado. 
+Faça uma validação dos dados por meio de uma outra função, permitindo que somente valores positivos sejam aceitos.
---
+++ bloco_3/utilitarios_bloco3.py
@@ -0,0 +1,54 @@
+#Funções exercícios1:
+def sucessor_antecessor(numero):
+    intervalo_numerico = 'Intervalo numérico: {'
+    for intervalo in range (numero - 2, numero+3):
+        intervalo_numerico += f'{intervalo}, '
+    return f'{intervalo_numerico[:-2]}' + "}"
+
+def linha(quantidade = 10):
+    return "=" * quantidade
+
+def cabecalho(texto,quantidade = 10):
+    return (f'{linha(quantidade)}\n'
+            f'{texto.center(quantidade)}\n'
+            f'{linha(quantidade)}')
+
+############################################################
+#Funções exercícios2:
+def fatorial(numero = 0):
+    if numero == 0:
+        return "0! = 1"
+    else:
+        intervalo = f'{numero}! = '
+        resultado = 1
+        for fator in range (numero, 0,-1):
+            intervalo += f'{fator} x '
+            resultado *= fator
+        return f'{intervalo[:-2]}= {resultado}'
+
+def validaInteiro(quantidade = 0):
+    tentativa = 0
+    while tentativa < 3:
+        try:
+            valor_numerico = int(input('Digite o número inteiro: '))
+            if valor_numerico >= 0:
+                return valor_numerico
+                break
+            else:
+                if tentativa < 2:
+                    print(linha(quantidade))
+                    print('Apenas valores positivos')
+        except:
+            if tentativa < 2:
+                print(linha(quantidade))
+                print('Insira apenas valores númericos inteiros.')
+                continue
+        finally:
+            tentativa += 1
+
+
+    if tentativa >= 3:
+        return 'não'
+
+###################################################
+
